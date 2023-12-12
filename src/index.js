81eed40d061ff773d66b95ed26fc2924f861506b
Daniel Polk <Danielpolk7@icloud.com>
2023-12-11 21:44:48 -0500

    final commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const inquirer = require('inquirer');
-inquirer
-  .prompt([{
+const img = document.getElementById('svg');
+const logoShape = data.logoShape;
+const titleInput = data.titleInput;
+const titleColor = data.titleColor;
+const shapeColor = data.shapeColor;
+
+const questions = [{
     name: "titleInput",
     type: "input",
     message: "What is the 3 letter title you want displayed in your logo?",
@@ -8,34 +13,41 @@ inquirer
       if (prompt.length <= 3) {
           return true
       } else {
-          console.log('\nEnter a title to continue')
+          console.log('\nEnter a 3 letter title to continue')
           return false;
       }
   }
     },
-  {
-    name: "titleColor",
-    type: "list",
-    message: "What color do you want your title to be?",
-    choices: ['red', 'blue', 'green','yellow','purple','orange']
-  },])
-  .then((answers) => {
-    if (answers.titleColor === 'red') {
-      let logoColor = red
-    } else if (answers.titleColor === 'blue') {
-      let logoColor = blue
-    } else if (answers.titleColor === 'green') {
-      let logoColor = green
-    } else if (answers.titleColor === 'yellow') {
-      let logoColor = yelllow
-    } else if (answers.titleColor === 'purple') {
-      let logoColor = purple
-    } else if (answers.titleColor === 'orange') {
-      let logoColor = orange
+    {
+      name: "logoShape",
+      type: "list",
+      message: "what shape do you want your logo to be?",
+      choices: ['Square', 'Circle', 'Triangle']
+    },
+    {
+      name: "titleColor",
+      type: "list",
+      message: "What color do you want your title to be?",
+      choices: ['red', 'blue', 'green','yellow','purple','orange']
+    }, 
+    {
+      name:'shapeColor',
+      type:'list',
+      message:'What color do you want the shape to be?',
+      choices: ['red', 'blue', 'green','yellow','purple','orange']
     }
-  })
-  .catch((err) => {
-    if (err) {
-      alert('Error')
-    };
-  });
+  ];
+
+  function init() {
+    inquirer.prompt(questions).then((data) => {
+        console.log(JSON.stringify(data, null, " "));
+    });
+}
+
+init();
+
+function renderSVG (data) {
+  img.innerHTML(data.logoShape)
+};
+
+renderSVG(data);
